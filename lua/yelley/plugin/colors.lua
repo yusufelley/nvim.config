1acab14cb1ca9cb9afe7ec5fd181c409dddc5e04
Yusuf Elley <yelley2001@gmail.com>
2024-08-22 00:45:29 -0400

    refactored color.lua files. Removed after/color and put all color code in lua/plugin/color.lua

--- lua/yelley/plugin/colors.lua
+++ lua/yelley/plugin/colors.lua
@@ -1,6 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    -- this function used to be in after and called Color 
+    -- I think that made it callable but I don't see a reason for this
+    config = function()
+        color = color or "tokyonight"
+        vim.cmd.colorscheme(color)
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    end
 }
