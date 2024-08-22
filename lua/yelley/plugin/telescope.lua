2afca1a3960da4a26f3bb79488c88b1aed0664af
Yusuf Elley <yelley2001@gmail.com>
2024-08-22 00:53:42 -0400

    Refactored telescope out of after and put everything in plugin

--- lua/yelley/plugin/telescope.lua
+++ lua/yelley/plugin/telescope.lua
@@ -1,6 +1,19 @@
 return {
     "nvim-telescope/telescope.nvim",
-     dependencies = {
-         {"nvim-lua/plenary.nvim"},
-     }
+    dependencies = {
+        {"nvim-lua/plenary.nvim"},
+    },
+    config = function()
+
+        local builtin = require('telescope.builtin')
+        -- Searches for files 
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        -- Searches for git files 
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- Searches for phrase i think
+        -- Had to brew install ripgrep for this to work
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+    end
 }
