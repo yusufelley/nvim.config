27b35b849f9d09a1dcf4572841de2a4a7edf551a
Yusuf Elley <yelley2001@gmail.com>
2024-08-22 01:35:55 -0400

    Fix lsp warning by reinstalling lsp with lsp-zero

--- lua/yelley/plugin/lsp.lua
+++ lua/yelley/plugin/lsp.lua
@@ -1,26 +1,8 @@
 return {
-    { "neovim/nvim-lspconfig" },
-    -- nvim package manager for installing lsps and other stuff
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup()
-
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-            })
-        end
-    },
     {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
 }
